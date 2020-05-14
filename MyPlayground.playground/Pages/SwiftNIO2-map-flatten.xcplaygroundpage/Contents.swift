b2a6a6a9d804f27802e68e51a50062651e6ff84f
Leo Dion <leogdion@brightdigit.com>
2020-05-14 09:57:27 -0400

    fixing swiftnio2

--- MyPlayground.playground/Pages/SwiftNIO2-map-flatten.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/SwiftNIO2-map-flatten.xcplaygroundpage/Contents.swift
@@ -14,13 +14,14 @@ let threadPool = MultiThreadedEventLoopGroup(numberOfThreads: 4)
 let eventLoop = threadPool.next()
 let jsonDecoder = JSONDecoder()
 
-let nioFutures = (1 ... 100).map {
+let nioFutures : [EventLoopFuture<String>] = (1 ... 100).map {
   Post.nioDownload(withId: $0, using: jsonDecoder, on: eventLoop).map {
     $0.title
   }
 }
 
-let nioFlattened = EventLoopFuture.whenAllSucceed(nioFutures, on: eventLoop)
+// fail-fast with whenAllSucceed vs fail-slow with whenAllCompleted
+let nioFlattened : EventLoopFuture<[String]> = EventLoopFuture.whenAllSucceed(nioFutures, on: eventLoop)
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
